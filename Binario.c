a2252a2ce237580cd4b707bc2fcb99b1889a907f
GustavoSousa2917 <166885704+GustavoSousa2917@users.noreply.github.com>
2024-04-15 17:16:24 -0300

    Binario.c

--- Binario.c
+++ Binario.c
@@ -1,34 +1,34 @@
 #include <stdio.h>
 
 void decimal_para_binario(int n) {
-    int numero_binario[32];
+    int numerobinario[32];
     int i = 0;
 
     while (n != 0) {
-        numero_binario[i] = n % 2;
+        numerobinario[i] = n % 2;
         n = n / 2;
         i++;
     }
 
     if(i % 4 != 0){
         while(i % 4 != 0){
-            numero_binario[i] = 0;
+            numerobinario[i] = 0;
             i++;
         }
     }
     for (int j = i - 1; j >= 0; j--) {
-        printf("%d", numero_binario[j]);
+        printf("%d", numerobinario[j]);
         if((j % 4) == 0)
             printf(" ");
     }
 }
 
 int main() {
-    int numero_decimal = 0;
+    int numerodecimal = 0;
 
-    scanf("%d", &numero_decimal);
-    printf("O binario equivalente de %d: ", numero_decimal);
-    decimal_para_binario(numero_decimal);
+    scanf("%d", &numerodecimal);
+    printf("O binario equivalente de %d: ", numerodecimal);
+    decimal_para_binario(numerodecimal);
     printf("\n");
 
     return 0;
